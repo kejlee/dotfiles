b88c8157f7d43380a57aa69898dbd7ee7eb8253f
kenny <kenjlee09@gmail.com>
2026-08-18 15:14:17 -0700

    configuring options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,108 +2,97 @@
 -- SECTION 1: OPTIONS
 -- Core Neovim settings, leaders, options, basic keymaps, basic autocmds
 -- ============================================================
-do
+-- clipboard stuff
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
+-- Enable faster startup by caching compiled Lua modules
+vim.loader.enable()
 
-  vim.g.clipboard = {
-    name = "win32yank",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
-  -- Enable faster startup by caching compiled Lua modules
-  vim.loader.enable()
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-  -- Set <space> as the leader key
-  -- See `:help mapleader`
-  --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-  vim.g.mapleader = ' '
-  vim.g.maplocalleader = ' '
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
 
-  -- Set to true if you have a Nerd Font installed and selected in the terminal
-  vim.g.have_nerd_font = false
+-- [[ Setting options ]]
+--  See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
 
-  -- [[ Setting options ]]
-  --  See `:help vim.o`
-  -- NOTE: You can change these options as you wish!
-  --  For more options, you can see `:help option-list`
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.showmode = false
 
-  -- Make line numbers default
-  vim.o.number = true
-  -- You can also add relative line numbers, to help with jumping.
-  --  Experiment for yourself to see if you like it!
-  vim.o.relativenumber = true
-  -- Enable mouse mode, can be useful for resizing splits for example!
-  vim.o.mouse = 'a'
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
-  -- Don't show the mode, since it's already in the status line
-  vim.o.showmode = false
+-- Enable break indent
+vim.o.breakindent = true
 
-  -- Sync clipboard between OS and Neovim.
-  --  Schedule the setting after `UiEnter` because it can increase startup-time.
-  --  Remove this option if you want your OS clipboard to remain independent.
-  --  See `:help 'clipboard'`
-  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.o.tabstop = 2      -- how wide a literal tab renders
+vim.o.shiftwidth = 2   -- indent width for >>, <<, autoindent, and LSP formatting
+vim.o.softtabstop = 2  -- how far Tab/Backspace move in insert mode
+vim.o.expandtab = true -- insert spaces instead of tab characters
 
-  -- Enable break indent
-  vim.o.breakindent = true
+-- Enable undo/redo changes even after closing and reopening a file
+vim.o.undofile = true
 
-  vim.o.tabstop = 2      -- how wide a literal tab renders
-  vim.o.shiftwidth = 2   -- indent width for >>, <<, autoindent, and LSP formatting
-  vim.o.softtabstop = 2  -- how far Tab/Backspace move in insert mode
-  vim.o.expandtab = true -- insert spaces instead of tab characters
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
-  -- Enable undo/redo changes even after closing and reopening a file
-  vim.o.undofile = true
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
 
-  -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-  vim.o.ignorecase = true
-  vim.o.smartcase = true
+-- Decrease update time
+vim.o.updatetime = 250
 
-  -- Keep signcolumn on by default
-  vim.o.signcolumn = 'yes'
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
 
-  -- Decrease update time
-  vim.o.updatetime = 250
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
 
-  -- Decrease mapped sequence wait time
-  vim.o.timeoutlen = 300
+-- disable swap files
+vim.o.swapfile = false
 
-  -- Configure how new splits should be opened
-  vim.o.splitright = true
-  vim.o.splitbelow = true
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-guide-options`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-  -- disable swap files
-  vim.o.swapfile = false
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
 
-  -- Sets how neovim will display certain whitespace characters in the editor.
-  --  See `:help 'list'`
-  --  and `:help 'listchars'`
-  --
-  --  Notice listchars is set using `vim.opt` instead of `vim.o`.
-  --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
-  --   See `:help lua-options`
-  --   and `:help lua-guide-options`
-  vim.o.list = true
-  vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Show which line your cursor is on
+vim.o.cursorline = true
 
-  -- Preview substitutions live, as you type!
-  vim.o.inccommand = 'split'
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
 
-  -- Show which line your cursor is on
-  vim.o.cursorline = true
-
-  -- Minimal number of screen lines to keep above and below the cursor.
-  vim.o.scrolloff = 10
-
-  -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
-  -- instead raise a dialog asking if you wish to save the current file(s)
-  -- See `:help 'confirm'`
-  vim.o.confirm = true
-end
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
 
